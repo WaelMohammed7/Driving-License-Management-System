31cba96ce12e88715cdb7260f909ba9d6a81aed2
WaelMohammed7 <zwlashhjhah@gmail.com>
2025-10-12 14:15:39 +0300

    Use Windows Authentication for safer connection

--- DVLD_DataAccess/clsDataAccessSettings.cs
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -4,7 +4,8 @@ namespace DVLD_DataAccess
 {
     static class clsDataAccessSettings
     {
-        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=123456;";
+        public static string ConnectionString = "Server=.;Database=DVLD;Integrated Security=True;";
+
 
 
     }
